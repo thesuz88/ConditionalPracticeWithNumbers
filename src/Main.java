dd65ece8342efe17ec2260504178d03e2a35695c
thesuz88 <stevensuzio@gmail.com>
2017-06-27 12:53:50 -0400

    Program will loop until number larger than 100 is entered

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,27 @@ public class Main {
 
         int userNum = 0;
 
-        System.out.println("Enter a number between 1 and 100: ");
-        userNum = scan.nextInt();
+        do {
+            System.out.println("Enter a whole number between 1 and 100: ");
+            while (!scan.hasNextInt()) {
+                String input = scan.next();
+                System.out.println(input + " is not a valid input. Enter a whole number between 1 and 100: ");
+            }
+            userNum = scan.nextInt();
 
-        if (userNum % 2 == 0) {
-            if (userNum >= 2 && userNum <= 24) {
-                System.out.println("Even and less than 25.");
+            if (userNum % 2 == 0) {
+                if (userNum >= 2 && userNum <= 24) {
+                    System.out.println("Even and less than 25.");
+                } else if (userNum >= 26 && userNum <= 60) {
+                    System.out.println("Even.");
+                } else {
+                    System.out.println(userNum + " Even.");
+                }
+            } else if (userNum % 2 != 0 && userNum > 60) {
+                System.out.println("Odd and over 60.");
+            } else {
+                System.out.println("Odd.");
             }
-            else if (userNum >= 26 && userNum <= 60) {
-                System.out.println("Even.");
-            }
-            else {
-                System.out.println(userNum + " Even.");
-            }
-        }
-        else if (userNum % 2 != 0 && userNum > 60){
-            System.out.println("Odd and over 60.");
-        }
-        else {
-            System.out.println("Odd.");
-        }
+        } while (userNum >= 1 && userNum <= 100);
     }
 }
